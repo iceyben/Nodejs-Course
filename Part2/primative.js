106ee53f4aa1f33e55effc0fb33beb245bae6886
Benjamin James Mulbah <mulbahb997@gmail.com>
2025-10-30 01:56:56 +0200

    first commit

---
+++ Part2/primative.js
@@ -0,0 +1,14 @@
+
+//types of data types: Primitive and Non-Primitive
+
+//Primitive data: [String, Boolean, Number, null, undefined, Symbol] 
+
+// Non-Primitive data: [Objects, array, functions,objects]
+
+// 1. Number:
+let balance = 120;
+let anotherBalance = new Number(120)
+console.log(typeof balance);
+console.log(balance);
+
+
